3cffa0ff7d81445625ddff86db8a7054e8c00f11
danajlim <danalim0819@gmail.com>
2025-01-04 15:32:49 +0900

    Initial Commit

---
+++ Nupi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Nupi
+//
+//  Created by Dana Lim on 1/4/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
