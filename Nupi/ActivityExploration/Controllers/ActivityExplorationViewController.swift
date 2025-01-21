f9ca5dd5dd6b3c982e2faae3297daa74810c5fca
kayeon2 <tjsrkdus@gmail.com>
2025-01-21 19:58:38 +0900

    ✨ Feat: 카테고리 컬렉션 뷰 추가

--- Nupi/ActivityExploration/Controllers/ActivityExplorationViewController.swift
+++ Nupi/ActivityExploration/Controllers/ActivityExplorationViewController.swift
@@ -8,6 +8,10 @@
 import UIKit
 
 class ActivityExplorationViewController: UIViewController {
+    
+    // 카테고리 배열
+    let categories = ["전시", "소품샵", "굿즈샵", "맛집", "카페", "테마카페", "팝업", "전시", "클래스"]
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,31 +21,71 @@ class ActivityExplorationViewController: UIViewController {
     
     private lazy var activityExplorationView: ActivityExplorationView = {
         let view = ActivityExplorationView()
-        view.backgroundColor = .white
+        view.backgroundColor = .bg
         return view
     }()
     
     private func setupDelegate() {
         activityExplorationView.bannerCollectionView.dataSource = self
+        activityExplorationView.categoryCollectionView.dataSource = self
+        activityExplorationView.categoryCollectionView.delegate = self
     }
 }
 
 extension ActivityExplorationViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return BannerModel.dummny().count
+        if collectionView == activityExplorationView.bannerCollectionView {
+            return BannerModel.dummny().count
+        } else if collectionView == activityExplorationView.categoryCollectionView {
+            return categories.count
+        }
+        
+        return 0
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let cell = collectionView.dequeueReusableCell(
-            withReuseIdentifier: BannerCollectionViewCell.identifier,
-            for: indexPath
-        ) as? BannerCollectionViewCell else {
-            return UICollectionViewCell()
+        if collectionView == activityExplorationView.bannerCollectionView {
+            guard let cell = collectionView.dequeueReusableCell(
+                withReuseIdentifier: BannerCollectionViewCell.identifier,
+                for: indexPath
+            ) as? BannerCollectionViewCell else {
+                return UICollectionViewCell()
+            }
+            
+            let list = BannerModel.dummny()
+            cell.bannerImageView.image = list[indexPath.row].image
+            return cell
+        } else if collectionView == activityExplorationView.categoryCollectionView {
+            guard let cell = collectionView.dequeueReusableCell(
+                withReuseIdentifier: CategoryCollectionViewCell.identifier,
+                for: indexPath
+            ) as? CategoryCollectionViewCell else {
+                return UICollectionViewCell()
+            }
+
+            cell.categoryButton.setTitle(categories[indexPath.row], for: .normal)
+            return cell
         }
         
-        let list = BannerModel.dummny()
-        cell.bannerImageView.image = list[indexPath.row].image
+        return UICollectionViewCell()
+    }
+}
+
+extension ActivityExplorationViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(
+        _ collectionView: UICollectionView,
+        layout collectionViewLayout: UICollectionViewLayout,
+        sizeForItemAt indexPath: IndexPath
+    ) -> CGSize {
+        let text = categories[indexPath.item]
+        let tmpLabel = UILabel()
+        tmpLabel.text = text
+        tmpLabel.font = UIFont(name: "WantedSans-Medium", size: 16)
+        tmpLabel.sizeToFit()
+
+        let width = tmpLabel.intrinsicContentSize.width
+        let height: CGFloat = 29
         
-        return cell
+        return CGSize(width: width + 24, height: height)
     }
 }
--- Nupi/ActivityExploration/Views/ActivityExplorationView.swift
+++ Nupi/ActivityExploration/Views/ActivityExplorationView.swift
@@ -23,9 +23,9 @@ class ActivityExplorationView: UIView {
     }
     
     // MARK: - Property
+    // 광고 배너 콜렉션 뷰
     let bannerCollectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout().then {
         $0.scrollDirection = .horizontal
-//        $0.estimatedItemSize = UICollectionViewFlowLayout.automaticSize
         $0.estimatedItemSize = .init(width: 337, height: 115)
         $0.minimumLineSpacing = 8
         $0.minimumInteritemSpacing = 0
@@ -34,10 +34,22 @@ class ActivityExplorationView: UIView {
         $0.showsHorizontalScrollIndicator = false
         $0.register(BannerCollectionViewCell.self, forCellWithReuseIdentifier: BannerCollectionViewCell.identifier)
     }
+
+    // 카테고리 콜렉션 뷰
+    let categoryCollectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout().then {
+        $0.scrollDirection = .horizontal
+//        $0.minimumLineSpacing = 0
+        $0.minimumInteritemSpacing = 8
+    }).then {
+        $0.backgroundColor = .clear
+        $0.showsHorizontalScrollIndicator = false
+        $0.register(CategoryCollectionViewCell.self, forCellWithReuseIdentifier: CategoryCollectionViewCell.identifier)
+    }
     
     // MARK: - Constaints & Add Function
     private func addComponents() {
         addSubview(bannerCollectionView)
+        addSubview(categoryCollectionView)
     }
     
     private func constraints() {
@@ -46,6 +58,13 @@ class ActivityExplorationView: UIView {
             $0.leading.equalToSuperview().offset(28)
             $0.trailing.equalToSuperview()
             $0.height.equalTo(115)
+        }
+        
+        categoryCollectionView.snp.makeConstraints {
+            $0.top.equalTo(bannerCollectionView.snp.bottom).offset(16)
+            $0.leading.equalToSuperview().offset(28)
+            $0.trailing.equalToSuperview().offset(-28)
+            $0.height.equalTo(29)
         }
     }
 }
