1a4817bfac115856afd2ada015fcce9fc7c4d418
nar0ng <71819594+nar0ng@users.noreply.github.com>
2025-01-25 13:06:27 +0900

    Revert "➕ Depend: KaKaoMapSDK 라이브러리 추가"
    This reverts commit 8138ef0ef809aaee2bbc68d63f530572ec528231.

--- Nupi/AppDelegate.swift
+++ Nupi/AppDelegate.swift
@@ -6,7 +6,6 @@
 //
 
 import UIKit
-import KakaoMapsSDK
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
--- Nupi/SceneDelegate.swift
+++ Nupi/SceneDelegate.swift
@@ -15,6 +15,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(frame: windowScene.coordinateSpace.bounds)
         window?.windowScene = windowScene
+        
         // UINavigationController를 사용하여 루트 설정
         let navigationController = UINavigationController()
         let coordinator = MainCoordinator(navigationController: navigationController)
@@ -54,4 +55,5 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
 }
+
 
