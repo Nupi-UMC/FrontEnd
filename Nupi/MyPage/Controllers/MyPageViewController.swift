b165b855ac2eae891bef525521d94c296ceeb4d0
김건 <gimgeon@gimgeon-ui-MacBookAir-2.local>
2025-02-15 16:14:18 +0900

    ✨ Feat: 프로필 콜렉션 뷰 추가

--- Nupi/MyPage/Controllers/MyPageViewController.swift
+++ Nupi/MyPage/Controllers/MyPageViewController.swift
@@ -1,14 +1,73 @@
 //
-//  ViewController.swift
+//  MyPageViewController.swift
 //  Nupi
 //
-//  Created by Dana Lim on 1/4/25.
+//  Created by 신연주 on 1/4/25.
 //
 
 import UIKit
 
 class MyPageViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view = myPageView
+        setupActions()
+        setupDelegates()
+        setupNavigationBar()
+    }
+    
+    private lazy var myPageView: MyPageView = {
+        let view = MyPageView()
+        return view
+    }()
+    
+    // MARK: - function
+    private func setupActions() {
+    }
+    
+    private func setupDelegates() {
+        myPageView.profileCollectionView.dataSource = self
+    }
+    
+    // 네비게이션바 추가
+    private func setupNavigationBar() {
+        self.navigationController?.navigationBar.barTintColor = .naviBar
+        self.navigationController?.navigationBar.tintColor = .icon1
+        self.navigationController?.navigationBar.topItem?.title = ""
+        
+        let titleLabel = UILabel().then {
+            $0.text = "MY Page"
+            $0.font = UIFont(name: "WantedSans-SemiBold", size: 17)
+            $0.textColor = .icon1
+        }
+        
+        self.navigationItem.titleView = titleLabel
+        self.navigationItem.hidesBackButton = true //네비게이션바 뒤로가기버튼 숨기기
+    }
+}
+    
+
+extension MyPageViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        if collectionView == myPageView.profileCollectionView{
+            return 1
+        }
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if collectionView == myPageView.profileCollectionView {
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ProfileCollectionViewCell.identifier, for: indexPath) as? ProfileCollectionViewCell else {
+                return UICollectionViewCell()
+            }
+            
+            cell.profileImageView.image = UIImage(named: "steady_popular_spot_image1") // 기본 프로필 이미지
+            cell.nicknameLabel.text = "스눕도기독"
+            cell.emailLabel.text = "arcticmon..."
+            
+            return cell
+        }
+        return UICollectionViewCell()
     }
 }
--- Nupi/MyPage/Views/MyPageView.swift
+++ Nupi/MyPage/Views/MyPageView.swift
@@ -1,20 +1,52 @@
 //
-//  HomeView.swift
+//  MyPageView.swift
 //  Nupi
 //
-//  Created by Dana Lim on 1/9/25.
+//  Created by 신연주 on 1/9/25.
 //
 
 import UIKit
+import SnapKit
 
 class MyPageView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = .bg
+        setViews()
     }
-    */
-
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: UI Components
+    
+    // 프로필 영역
+    let profileCollectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout().then{
+        $0.estimatedItemSize = .init(width: 345, height: 80)}).then{
+            $0.backgroundColor = .clear
+            $0.showsHorizontalScrollIndicator = false
+            $0.register(ProfileCollectionViewCell.self, forCellWithReuseIdentifier: ProfileCollectionViewCell.identifier)
+        }
+    
+    // 버튼 영역
+    
+    // 상단 구분선
+    
+    
+    // 메뉴 영역
+    
+    // 하단 구분선
+    // MARK: 컴포넌트 추가
+    private func setViews(){
+        addSubview(profileCollectionView)
+        //addSubview(separatorLine)
+        
+        profileCollectionView.snp.makeConstraints{
+            $0.top.equalTo(safeAreaLayoutGuide).offset(11)
+            $0.left.right.equalToSuperview().inset(24)
+            $0.height.equalTo(80)
+        }
+    }
 }
